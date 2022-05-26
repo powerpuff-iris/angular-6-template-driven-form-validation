4d33270ac64937977d85c6c827ab4ae5af1cdb56
powerpuff-iris <irisxhemollari@gmail.com>
2022-05-26 15:37:52 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-6-template-driven-form-validation-sddzfy
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-6-template-driven-form-validation-sddzfy)
---
+++ app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app',
+  templateUrl: 'app.component.html',
+})
+export class AppComponent {
+  model: any = {};
+
+  onSubmit() {
+    alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.model));
+  }
+  log(x: string) {
+    console.log(x);
+  }
+}
